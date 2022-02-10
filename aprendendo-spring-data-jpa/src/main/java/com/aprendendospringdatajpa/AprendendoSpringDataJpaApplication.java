a2018e60a02a0a3b18b5fcd0e4b8eac73a4af4ce
caducarlos00 <caducarlos123@gmail.com>
2022-02-09 22:01:11 -0300

    Criando projeto para estudar Spring Data JPA

---
+++ aprendendo-spring-data-jpa/src/main/java/com/aprendendospringdatajpa/AprendendoSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.aprendendospringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprendendoSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprendendoSpringDataJpaApplication.class, args);
+	}
+
+}
